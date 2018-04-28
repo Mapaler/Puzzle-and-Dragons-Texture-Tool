a9b5ca322b0609f223d23d5d066f3ae8a59c6bd4
枫谷剑仙 <mapaler@163.com>
2018-04-28 22:59:47 +0800

    解包用文件

---
+++ 提取所有Data图片.bat
@@ -0,0 +1,7 @@
+@echo off
+title 提取所有Data图片
+REM  %time:~,2%h%time:~3,2%m%time:~6,2%s
+PADTextureTool.py D:\CTFX\Documents\PadFiles\files-last\mon2\ --outdir "Data_%date:~,4%-%date:~5,2%-%date:~8,2%" -nt
+title 提取Data完成
+echo 提取完成
+pause
