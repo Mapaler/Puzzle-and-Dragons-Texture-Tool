7752a6173285c2acdff18eba6ffdd530aff095ae
枫谷剑仙 <mapaler@163.com>
2019-11-08 17:49:04 +0800

    增加英文和韩文的提取

--- 转换最新提交修改文件的图.bat
+++ 转换最新提交修改文件的图.bat
@@ -1,10 +1,2 @@
 @echo off
-set opath=%~dp0
-set repository=C:\Users\CTFX\Nox_share\OtherShare\files\
-cd /d %repository%
-git config --global i18n.commit.encoding utf-8
-git config --global i18n.logoutputencoding gbk
-for /f %%i in ('git show --name-only --pretty^=') do (
-cd /d %opath%
-python PADTextureTool.py %repository%%%i -o temp -nt -nb
-)
+cscript //Nologo 转换最新提交修改文件的图.js %UserProfile%\Nox_share\OtherShare\files\
