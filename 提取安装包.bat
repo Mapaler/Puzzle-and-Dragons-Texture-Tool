40163fe95f1f7d738e191952228a492cfb109aca
枫谷剑仙 <mapaler@163.com>
2024-06-20 20:17:31 +0800

    提取最新时跳过音乐文件

--- 提取安装包.bat
+++ 提取安装包.bat
@@ -1,7 +1,7 @@
 @echo off
 title 提取所有APK图片
 REM  %time:~,2%h%time:~3,2%m%time:~6,2%s
-python PADTextureTool.py %UserProfile%\Nox_share\AppShare\jp.gungho.pad.apk --outdir "APK_%date:~,4%-%date:~5,2%-%date:~8,2%" -nt -nb
+python PADTextureTool.py C:\ProgramData\BlueStacks_nxt\Engine\UserData\SharedFolder\パズドラ_21.6.0.APK --outdir "APK_%date:~,4%-%date:~5,2%-%date:~8,2%" -nt -nb
 title 提取APK完成
 echo 提取完成
 pause
--- 提取所有Data图片.bat
+++ 提取所有Data图片.bat
@@ -1,7 +1,7 @@
 @echo off
 title 提取所有Data图片
 REM  %time:~,2%h%time:~3,2%m%time:~6,2%s
-python PADTextureTool.py %UserProfile%\Nox_share\OtherShare\files\mon2\ --outdir "Data_%date:~,4%-%date:~5,2%-%date:~8,2%" -nt -nb
+python PADTextureTool.py D:\CTFX\Downloads\Game\PAD\Puzzle-and-Dragons-Texture-Tool\tencentzlmcs\assets\mon2\ --outdir "Data_%date:~,4%-%date:~5,2%-%date:~8,2%" -nt -nb
 title 提取Data完成
 echo 提取完成
 pause
